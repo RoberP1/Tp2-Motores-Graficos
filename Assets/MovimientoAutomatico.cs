0e086fe1cdec41d32360e722d3b05ce23f38549c
RoberP1 <roberpapeschi@gmail.com>
2021-09-25 04:29:06 -0300

    Movimiento Piso Rojo

---
+++ Assets/MovimientoAutomatico.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoAutomatico : MonoBehaviour
+{
+    public bool tengoQueBajar = false;
+    public float rapidez = 1;
+    public float MaxArriba = 5;
+    public float MaxAbajo = 2;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (transform.position.y >= MaxArriba)
+        {
+            tengoQueBajar = true;
+        }
+        if (transform.position.y <= MaxAbajo)
+        {
+            tengoQueBajar = false;
+        }
+
+        if (tengoQueBajar)
+        {
+            Bajar();
+        }
+        else
+        {
+            Subir();
+        }
+
+    }
+
+    void Subir()
+    {
+        //transform.position += transform.up * rapidez *Time.deltaTime ;
+        rb.velocity = transform.up * rapidez;
+        
+    }
+
+    void Bajar()
+    {
+        //transform.position -= transform.up * rapidez * Time.deltaTime;
+        rb.velocity = -transform.up * rapidez;
+    }
+
+}
