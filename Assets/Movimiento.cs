6a984f2d255d992fcabe98f2d1c451d89c0dadf4
RoberP1 <roberpapeschi@gmail.com>
2021-09-25 17:30:30 -0300

    Mejora de Físicas Movimiento

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -8,8 +8,11 @@ public class Movimiento : MonoBehaviour
 
     public float velocidad = 1;
     public float VelocidadRotacion = 1;
+    public float MaxVel = 3;
     private Rigidbody rb;
     public GameObject camara;
+    Vector3 Vel =new Vector3(0,0,0);
+    
     
 
 
@@ -20,30 +23,40 @@ public class Movimiento : MonoBehaviour
 
 
     void Update()
-    { 
+    {
+        Vel.x = rb.velocity.x;
+        Vel.z = rb.velocity.z;
         if (Input.GetMouseButton(1))
         {
             rb.transform.rotation = new Quaternion(rb.transform.rotation.x, camara.transform.rotation.y, rb.transform.rotation.z, camara.transform.rotation.w);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && (Vel.x < MaxVel && Vel.x > -MaxVel))
         {
             rb.transform.rotation = new Quaternion(rb.transform.rotation.x, camara.transform.rotation.y, rb.transform.rotation.z, camara.transform.rotation.w);
 
-            rb.position += transform.forward * velocidad * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
+           
+            rb.AddRelativeForce(Vector3.forward * velocidad, ForceMode.Force);
+
+            //rb.position += transform.forward * velocidad * Time.deltaTime;
+        } 
+        if (Input.GetKey(KeyCode.S) && (Vel.x < MaxVel && Vel.x > -MaxVel))
         {
             rb.transform.rotation = new Quaternion(rb.transform.rotation.x, camara.transform.rotation.y, rb.transform.rotation.z, camara.transform.rotation.w);
 
-            rb.position += -transform.forward * velocidad * Time.deltaTime;
+            //rb.position += -transform.forward * velocidad * Time.deltaTime;
+            rb.AddRelativeForce(Vector3.forward * -velocidad, ForceMode.Force);
+
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && (Vel.z < MaxVel && Vel.z > -MaxVel))
         {
-            rb.position += transform.right * velocidad * Time.deltaTime;
+            //rb.position += transform.right * velocidad * Time.deltaTime;
+            rb.AddRelativeForce(Vector3.right * velocidad, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && (Vel.z < MaxVel && Vel.z > -MaxVel))
         {
-            rb.position += -transform.right * velocidad * Time.deltaTime;
+            //rb.position += -transform.right * velocidad * Time.deltaTime;
+            rb.AddRelativeForce(Vector3.right * -velocidad, ForceMode.Force);
         }
+       
     }
 }
